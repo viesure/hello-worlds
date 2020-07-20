b5c8d05dd0edeaf71d1d0a635a8800f8d4c74fcf
tamastoth <tamas.laszlo.toth@viesure.io>
2020-07-20 15:04:18 +0200

    Microcontroller helloworld

---
+++ micro_controller/Hello_world_example.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  Serial.println("Hello world")
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
